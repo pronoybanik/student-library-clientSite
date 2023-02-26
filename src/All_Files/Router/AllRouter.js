3f0425b8dc1bb9173e42382a4b10dbecf69c0c82
PRONOY <pronoybanik82@gmail.com>
2023-02-27 00:04:38 +0600

    login page and register page degine done

--- src/All_Files/Router/AllRouter.js
+++ src/All_Files/Router/AllRouter.js
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layers/Main/Main";
 import Home from "../Pages/Home/Home";
+import Register from "../Pages/Register/Register";
+import Login from "../Pages/Login/Login"
 
 
 const AllRouter = createBrowserRouter([
@@ -11,6 +13,14 @@ const AllRouter = createBrowserRouter([
             {
                 path: '/',
                 element: <Home></Home>
+            },
+            {
+                path: '/register',
+                element: <Register></Register>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
             },
         ]
     }
