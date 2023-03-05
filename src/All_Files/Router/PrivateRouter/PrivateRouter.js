ab3462b5318b7d6e94f07150cfd84b51b526faf2
EMHOSSAIN <me288715@gmail.com>
2023-03-05 21:37:02 +0600

    worked to fetch Data in home page

---
+++ src/All_Files/Router/PrivateRouter/PrivateRouter.js
@@ -0,0 +1,17 @@
+import React, { useContext } from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+import { AuthContext } from '../../Context/AuthProvider';
+
+const PrivateRouter = ({children}) => {
+    const {user,loading}=useContext(AuthContext)
+    const location = useLocation()
+    if(loading){
+      return  <button className='btn'>loading........</button>
+    }
+    if(!user){
+        return<Navigate to='/login' state={{from:location}} replace></Navigate>
+    }
+    return children
+};
+
+export default PrivateRouter;
